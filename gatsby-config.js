3c656ab28ad39a8f42440a6b2db8ed1127f0b7b0
”luyangk“ <luyangk@gmail.com>
2022-07-11 21:20:16 +0800

    add gitignore

---
+++ gatsby-config.js
@@ -0,0 +1,72 @@
+module.exports = {
+  pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
+  siteMetadata: {
+    siteTitle: 'Lucca\'s Running Page',
+    siteUrl: 'https://vercel.com/luccalu',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    description: 'Personal Running Page',
+    navLinks: [
+      {
+        name: 'Vlog',
+        url: 'https://vercel.com/luccalu/running',
+      },
+      {
+        name: 'About',
+        url: 'https://vercel.com/luccalu/about',
+      },
+    ],
+  },
+  plugins: [
+    'gatsby-transformer-json',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/static/',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/images/',
+      },
+    },
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        rootFolder: './',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        cssLoaderOptions: {
+          esModule: false,
+          modules: {
+            namedExport: false,
+          },
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#e1e1e1',
+        theme_color: '#e1e1e1',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    }
+  ],
+};
